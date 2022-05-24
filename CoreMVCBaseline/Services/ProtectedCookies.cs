571452cb1c7177ae288919c452bd0aac7ca9f2c2
Alexis Triemstra <alexis.triemstra.uk@gmail.com>
2022-05-24 17:06:23 +0100

    Added ProtectedCookies service

---
+++ CoreMVCBaseline/Services/ProtectedCookies.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCBaseline.Services
+{
+    public class ProtectedCookies : IProtectedCookies
+    {
+        private readonly IDataProtector _protector;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ProtectedCookies(IDataProtectionProvider provider, IHttpContextAccessor httpContextAccessor)
+        {
+            _protector = provider.CreateProtector("CookieProtector");
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string Get(string key)
+        {
+            return _protector.Unprotect(_httpContextAccessor.HttpContext.Request.Cookies[key]??string.Empty);
+        }
+
+        public void Set(string key, string value)
+        {
+            _httpContextAccessor.HttpContext.Response.Cookies.Append(key, _protector.Protect(value), new CookieOptions { Expires = DateTime.Now.AddMinutes(60), IsEssential = true });
+        }
+
+    }
+}
