fad47708567f6df3dc7560fb18e899a2ff0cdb9a
Alexis Triemstra <alexis.triemstra.uk@gmail.com>
2022-10-12 22:38:32 +0100

    Added ApiKey and Swagger

---
+++ CoreMVCBaseline/MIddleware/ApiKeyMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using CoreMVCBaseline.Configuration;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMVCBaseline.MIddleware
+{
+    public class ApiKeyMiddleware
+    {
+        private readonly RequestDelegate _next;        
+        public ApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var apiKeyConfiguration = new ApiKeyConfiguration();
+            var configuration = context.RequestServices.GetRequiredService<IConfiguration>();
+            configuration.Bind(nameof(ApiKeyConfiguration), apiKeyConfiguration);
+            
+
+            if (!context.Request.Headers.TryGetValue(apiKeyConfiguration.ApiHeader, out var extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Api Key was not provided ");
+                return;
+            }
+            
+
+            if (!apiKeyConfiguration.XApiKey.Equals(extractedApiKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Unauthorized client");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
--- CoreMVCBaseline/Startup.cs
+++ CoreMVCBaseline/Startup.cs
@@ -1,9 +1,14 @@
+using CoreMVCBaseline.Configuration;
+using CoreMVCBaseline.MIddleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Serilog;
+using System;
+using System.Collections.Generic;
 
 namespace CoreMVCBaseline
 {
@@ -27,6 +32,44 @@ namespace CoreMVCBaseline
 
             services.AddHttpContextAccessor();
             services.AddCustomServices(Configuration);
+            services.AddSwaggerGen();
+
+            var apiKeyConfiguration = new ApiKeyConfiguration();
+            Configuration.Bind(nameof(ApiKeyConfiguration), apiKeyConfiguration);
+
+            if (string.IsNullOrEmpty(apiKeyConfiguration?.ApiHeader))
+                throw new InvalidOperationException("ApiKeyConfiguration.ApiHeader is null or empty.");
+
+            services.AddSwaggerGen(c =>
+            {
+                const string securityDefinition = "ApiKey";
+
+                // https://stackoverflow.com/questions/36975389/api-key-in-header-with-swashbuckle
+                c.AddSecurityDefinition(securityDefinition, new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Name = apiKeyConfiguration.ApiHeader,
+                    Type = SecuritySchemeType.ApiKey
+                });
+                // https://stackoverflow.com/questions/57227912/swaggerui-not-adding-apikey-to-header-with-swashbuckle-5-x
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Name = apiKeyConfiguration.ApiHeader,
+                            Type = SecuritySchemeType.ApiKey,
+                            In = ParameterLocation.Header,
+                            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = securityDefinition }
+                        },
+                        new List<string>()
+                    }
+                });
+
+                //var xmlFile = $"{assemblyName}.xml";
+                //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                //c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +78,8 @@ namespace CoreMVCBaseline
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
             else
             {
@@ -42,12 +87,20 @@ namespace CoreMVCBaseline
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
+
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
-
             app.UseAuthorization();
+
+
+            app.UseWhen(context => context.Request.Path.StartsWithSegments("/api"), appBuilder =>
+            {
+                appBuilder.UseMiddleware<ApiKeyMiddleware>();
+            });
 
             app.UseEndpoints(endpoints =>
             {
@@ -55,6 +108,7 @@ namespace CoreMVCBaseline
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+
         }
     }
 }
